90ccc9497763bae4ba561d97d4655db11541644c
17Sujith <120181005+17Sujith@users.noreply.github.com>
2023-03-23 21:27:23 +0530

    Create OpenCV.py

---
+++ OpenCV.py
@@ -0,0 +1,23 @@
+import cv2
+
+video = cv2.VideoCapture('22.mp4')
+if not video.isOpened():
+    print("Error opening video file")
+
+frame_skip_interval = 4
+fps=int(video.get(cv2.CAP_PROP_FPS))
+
+while True:
+    ret, frame = video.read()
+
+    if not ret:
+        break
+
+    cv2.imshow('Video', frame)
+    cv2.waitKey(1)
+
+    for i in range(0,fps,frame_skip_interval):
+        video.grab()
+
+video.release()
+cv2.destroyAllWindows()
